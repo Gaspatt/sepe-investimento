2f3b0e4ac87cfe4ff6c6b0fd0a99955fe7f3c693
Jaotarzan <joaovssouza59@gmail.com>
2023-10-21 01:15:19 -0300

    fecho

---
+++ js/calculadora.js
@@ -0,0 +1,24 @@
+function simular() {
+    let valorInicial = Number(document.getElementById('valorApp').value)
+    let aporte = Number(document.getElementById('aporteMensal').value);
+    let prazo = Number(document.getElementById('prazo').value);
+    let medida = document.querySelector('select').value;
+    let juros = Number(document.getElementById('juros').value);
+    let valorFinal = valorInicial;
+    if (medida == "1") {
+        for (let i = 0; i < prazo; i++) {
+            valorFinal += valorFinal * (juros / 100);
+            valorFinal += aporte;
+        }
+    }
+    else {
+        prazo *= 12;
+        for (let i = 0; i < prazo; i++) {
+            valorFinal += valorFinal * (juros / 100);
+            valorFinal += aporte;
+        }
+    }
+    console.log(medida)
+    console.log(valorFinal)
+    document.getElementById('resultado').innerHTML = 'R$' + valorFinal.toFixed(2).replace('.',',')
+}
